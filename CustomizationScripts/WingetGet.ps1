27f2bb3b292b0b2db5a4b5e1bb1bdecf287ee268
Scott McDonnell <94635857+HedgeComp@users.noreply.github.com>
2025-07-15 16:51:31 -0400

    Update WingetGet.ps1

--- CustomizationScripts/WingetGet.ps1
+++ CustomizationScripts/WingetGet.ps1
@@ -1,3 +1,4 @@
+$ProgressPreference = 'SilentlyContinue'
 $msix = "$env:TEMP\DesktopAppInstaller.msixbundle"
 Invoke-WebRequest `
   -Uri 'https://github.com/microsoft/winget-cli/releases/download/v1.11.400/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle' `

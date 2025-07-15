61871452ba9b2954b4a0a71d6348241f238eb007
Scott McDonnell <94635857+HedgeComp@users.noreply.github.com>
2025-07-15 16:59:15 -0400

    Update WingetGet.ps1

--- CustomizationScripts/WingetGet.ps1
+++ CustomizationScripts/WingetGet.ps1
@@ -4,12 +4,9 @@ Invoke-WebRequest `
   -Uri 'https://github.com/microsoft/winget-cli/releases/download/v1.11.400/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle' `
   -OutFile $msix
 
+Add-AppxProvisionedPackage -Online -PackagePath $msix -SkipLicense
 
-
-  Add-AppxProvisionedPackage `
-  -Online `
-  -PackagePath "$env:TEMP\Microsoft.DesktopAppInstaller.msixbundle" `
-  -SkipLicense
+& 'C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\winget.exe' --version
 
 # Registers App Installer so winget.exe becomes a “real” Win32 app available to every account
 Add-AppxPackage -Path $msix -Register -DisableDevelopmentMode -AllUsers

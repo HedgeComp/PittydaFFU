c271a387706a770110851f123a6910ff1c36de7b
Scott McDonnell <94635857+HedgeComp@users.noreply.github.com>
2025-07-15 17:05:10 -0400

    Update WingetGet.ps1

--- CustomizationScripts/WingetGet.ps1
+++ CustomizationScripts/WingetGet.ps1
@@ -10,3 +10,27 @@ Add-AppxProvisionedPackage -Online -PackagePath $msix -SkipLicense
 
 # Registers App Installer so winget.exe becomes a “real” Win32 app available to every account
 Add-AppxPackage -Path $msix -Register -DisableDevelopmentMode -AllUsers
+
+
+
+
+
+# 1. Path to the Winget stub
+$wingetExe = 'C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\winget.exe'
+
+# 2. Start the process with redirected streams
+$psi = New-Object System.Diagnostics.ProcessStartInfo
+$psi.FileName               = $wingetExe
+$psi.Arguments              = '--version'
+$psi.RedirectStandardOutput = $true
+$psi.RedirectStandardError  = $true
+$psi.UseShellExecute        = $false
+
+$proc = [System.Diagnostics.Process]::Start($psi)
+$proc.WaitForExit()
+
+# 3. Read back whatever was written
+$out = $proc.StandardOutput.ReadToEnd()
+$err = $proc.StandardError.ReadToEnd()
+
+"`nSTDOUT:`n$out", "`nSTDERR:`n$err" | ForEach-Object { Write-Host $_ }
